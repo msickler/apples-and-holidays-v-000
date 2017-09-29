b1f49a55824f232a1faf1ec75016728e8ca973c5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 16:52:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, supplies|
-    supplies.find_all {|supply| supply.include?("BBQ")}
+     supplies.include?("BBQ")
  holiday_hash
  end
  end
